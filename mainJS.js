dd6e6ae85cc5b5ccc425f9df36960c43cf5b511b
WinterCricket <hodhodhod2003@gmail.com>
2018-08-25 12:32:57 -0500

    use DOM manip appendC node para

--- mainJS.js
+++ mainJS.js
@@ -12,18 +12,38 @@
 
 //innerHTML
 
-var myText = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. <h1>Consectetur, itaque.</h1>";
-var targetDiv = document.getElementById('theDiv');
+// var myText = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. <h1>Consectetur, itaque.</h1>";
+// var targetDiv = document.getElementById('theDiv');
 //targetDiv.innerHTML = myText;
-targetDiv.innerHTML = targetDiv.innerHTML + myText;
+// targetDiv.innerHTML =  myText + targetDiv.innerHTML;
 
 
 
-//DOM manipulation methods
+//DOM manipulation methods for greater control
+
+function addNewContent() {
+
+	var para = document.createElement('p'); //create a tag
+	var node = document.createTextNode('Willows are funeral trees, rife in Little Venice.');//add text to new tag
+	para.appendChild(node);
+
+//add new tag etc to div
+
+var targetDiv = document.getElementById('theDiv');
+targetDiv.appendChild(para);
+console.log(targetDiv.innerHTML);
+
+}
+
+function domWork() {
+	var targetDiv = document.getElementById('theDiv');
+	//targetDiv.style.background = #bbbbf1;
+	console.log(targetDiv.innerHTML);
+}
 
 
 document.getElementById('actButton').onclick=function(){
-	myFunction();
+	addNewContent();
 }
 
 
