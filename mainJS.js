6756a727e923686e263db261de0e5fd9b35f435f
WinterCricket <hodhodhod2003@gmail.com>
2018-08-25 11:50:40 -0500

    use inner html for inserting

--- mainJS.js
+++ mainJS.js
@@ -3,18 +3,23 @@
 //switch to an argument
 //old school
 
-function myFunction() {
+// function myFunction() {
 
-	document.write("Listen, look, read, then write for you life!");
-}
+// 	document.write("Listen, look, read, then write for you life!");
+// }
 
 
 
 //innerHTML
 
-var myText = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur, itaque.";
+var myText = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. <h1>Consectetur, itaque.</h1>";
 var targetDiv = document.getElementById('theDiv');
-//targetDiv.innerHTML = targetDiv.innerHTML + myText;
+//targetDiv.innerHTML = myText;
+targetDiv.innerHTML = targetDiv.innerHTML + myText;
+
+
+
+//DOM manipulation methods
 
 
 document.getElementById('actButton').onclick=function(){
